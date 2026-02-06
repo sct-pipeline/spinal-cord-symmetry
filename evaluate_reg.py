7887e368cfd05d8c3aecea1a583b43bfeca58c82
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2026-02-06 14:14:53 -0500

    modify function

--- evaluate_reg.py
+++ evaluate_reg.py
@@ -4,87 +4,95 @@
 
 from __future__ import division, absolute_import
 import sys, os
-import sct_utils as sct
-from msct_parser import Parser
-from sct_register_to_template import main as sct_register_to_template
-from sct_label_vertebrae import main as sct_label_vertebrae
-from sct_apply_transfo import main as sct_apply_transfo
-from sct_label_utils import main as sct_labels_utils
-from sct_maths import main as sct_maths
-from nicolas_scripts.functions_sym_rot import *
-from spinalcordtoolbox.reports.qc import generate_qc
+import argparse
+from functions_sym_rot import *
 import csv
 
+
 def get_parser():
-
-    parser = Parser(__file__)
-    parser.usage.set_description('Script to process a MRI image with its segmentation, blablabla what does this script do')
-    parser.add_option(name="-i",
-                      type_value="file",
-                      description="File input",
-                      mandatory=True,
-                      example="/home/data/cool_T2_MRI.nii.gz")
-    parser.add_option(name="-iseg",
-                      type_value="file",
-                      description="Segmentation of the input file",
-                      mandatory=True,
-                      example="/home/data/cool_T2_MRI_seg_manual.nii.gz")
-    parser.add_option(name="-o",
-                      type_value="folder",
-                      description="output folder for test results",
-                      mandatory=False,
-                      example="path/to/output/folder")
-    parser.add_option(name='-qc',
-                      type_value='folder_creation',
-                      description='The path where the quality control generated content will be saved',
-                      mandatory=False)
-
+    parser = argparse.ArgumentParser(
+        description='Script to process a MRI image with its segmentation.'
+    )
+    parser.add_argument(
+        "-i",
+        type=str,
+        required=True,
+        help="File input, e.g. /home/data/cool_T2_MRI.nii.gz"
+    )
+    parser.add_argument(
+        "-iseg",
+        type=str,
+        required=True,
+        help="Segmentation of the input file, e.g. /home/data/cool_T2_MRI_seg_manual.nii.gz"
+    )
+    parser.add_argument(
+        "-sct-dir",
+        type=str,
+        required=True,
+        help="Path to SCT directory, e.g. /home/data/spinalcordtoolbox"
+    )
+    parser.add_argument(
+        "-o",
+        type=str,
+        required=False,
+        help="Output folder for test results, e.g. path/to/output/folder"
+    )
+    parser.add_argument(
+        "-qc",
+        type=str,
+        required=False,
+        help="The path where the quality control generated content will be saved"
+    )
     return parser
 
 def main(args=None):
-
     # Parser :
     if not args:
         args = sys.argv[1:]
     parser = get_parser()
-    arguments = parser.parse(args)
-    fname_image = arguments['-i']
-    fname_seg = arguments['-iseg']
-    if '-qc' in arguments:
-        path_qc = arguments['-qc']
+    arguments = parser.parse_args()
+    fname_image = arguments.i
+    fname_seg = arguments.iseg
+    sct_path = arguments.sct_dir
+    if arguments.qc:
+        path_qc = arguments.qc
         # creating qc dir if it does not exist
         if not os.path.isdir(path_qc):
             os.mkdir(path_qc)
-    if '-o' in arguments:
-        output_dir = arguments['-o']
+    if arguments.o:
+        output_dir = arguments.o
         # creating output dir if it does not exist
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
 
     methods = ["NoRot", "pca", "hog", "auto"]
 
-    fname_seg_template = os.path.join(sct.__data_dir__, 'PAM50/template/PAM50_cord.nii.gz')
+    fname_seg_template = os.path.join(sct_path, 'data/PAM50/template/PAM50_cord.nii.gz')
 
     sct.printv("        Python processing file : " + fname_image + " with seg : " + fname_seg)
 
     # Determining contrast :
     if ("T1w" in fname_image) or ("t1w" in fname_image) or ("MToff" in fname_image):
         contrast, contrast_label = "t1", "t1"
-        fname_image_template = os.path.join(sct.__data_dir__, "PAM50/template/PAM50_t1.nii.gz")
+        fname_image_template = os.path.join(sct_path, "data/PAM50/template/PAM50_t1.nii.gz")
     elif ("T2w" in fname_image) or ("t2w" in fname_image)or ("MTon" in fname_image):
         contrast, contrast_label = "t2", "t2"
-        fname_image_template = os.path.join(sct.__data_dir__, "PAM50/template/PAM50_t2.nii.gz")
+        fname_image_template = os.path.join(sct_path, "data/PAM50/template/PAM50_t2.nii.gz")
     elif ("T2s" in fname_image) or ("t2s" in fname_image):
         contrast, contrast_label = "t2s", "t2"
-        fname_image_template = os.path.join(sct.__data_dir__, "PAM50/template/PAM50_t2s.nii.gz")
+        fname_image_template = os.path.join(sct_path, "data/PAM50/template/PAM50_t2s.nii.gz")
     else:
         sct.printv("Contrast not supported yet for file : " + fname_image)
         return
 
     # Labelling vertebrae :
-    sct_label_vertebrae(['-i', fname_image, '-s', fname_seg, '-c', contrast_label, '-ofolder', output_dir, '-v', '0'])
+    os.system(
+        f'sct_label_vertebrae -i {fname_image} -s {fname_seg} -c {contrast_label} -ofolder {output_dir} -v 0'
+    )
     label_max = np.max(Image(output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_labeled.nii.gz").data)
-    sct_labels_utils(['-i', output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_labeled.nii.gz", '-vert-body', "1," + str(int(label_max)), '-o', output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_indiv_labels.nii.gz", '-v', str(0)])
+    os.system(
+        f'sct_label_utils -i {output_dir}/{(fname_seg.split("/")[-1]).split(".nii.gz")[0]}_labeled.nii.gz -vert-body 1,{int(label_max)} -o {output_dir}/{(fname_seg.split("/")[-1]).split(".nii.gz")[0]}_indiv_labels.nii.gz -v 0'
+    )
 
     # Applying same process but for different methods :
 
@@ -94,20 +102,21 @@ def main(args=None):
 
         # Registration
         if method == "NoRot":
-            sct_register_to_template(
-                ['-i', fname_image, '-s', fname_seg, '-c', contrast, '-l',
-                 output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_indiv_labels.nii.gz", '-ofolder',
-                 output_dir, '-param',
-                 "step=1,type=seg,algo=centermass,poly=0,slicewise=1", '-v', '0'])
+            os.system(
+                f'sct_register_to_template -i {fname_image} -s {fname_seg} -c {contrast} -l {output_dir}/{(fname_seg.split("/")[-1]).split(".nii.gz")[0]}_indiv_labels.nii.gz -ofolder {output_dir} -param "step=1,type=seg,algo=centermass,poly=0,slicewise=1" -v 0 -qc {path_qc}'
+            )
         else:
-            sct_register_to_template(
-                ['-i', fname_image, '-s', fname_seg, '-c', contrast, '-l',
-                 output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_indiv_labels.nii.gz", '-ofolder', output_dir, '-param',
-                 "step=1,type=seg,algo=centermassrot,poly=0,slicewise=1,rot_method=" + method, '-v', '0'])
+            os.system(
+                f'sct_register_to_template -i {fname_image} -s {fname_seg} -c {contrast} -l {output_dir}/{(fname_seg.split("/")[-1]).split(".nii.gz")[0]}_indiv_labels.nii.gz -ofolder {output_dir} -param "step=1,type=seg,algo=centermassrot,poly=0,slicewise=1,rot_method={method}" -v 0 -qc {path_qc}'
+            )
 
         # Applying warping field to segmentation
-        sct_apply_transfo(['-i', fname_seg, '-d', fname_seg_template, '-w', output_dir + "/warp_anat2template.nii.gz", '-o', output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_reg.nii.gz", '-v', str(0)])
-        sct_maths(['-i', output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_reg.nii.gz", '-bin', '0.5', '-o', output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_reg_tresh.nii.gz", '-v', str(0)])
+        os.system(
+            f'sct_apply_transfo -i {fname_seg} -d {fname_seg_template} -w {output_dir}/warp_anat2template.nii.gz -o {output_dir}/{(fname_seg.split("/")[-1]).split(".nii.gz")[0]}_reg.nii.gz -v 0'
+        )
+        os.system(
+            f'sct_maths -i {output_dir}/{(fname_seg.split("/")[-1]).split(".nii.gz")[0]}_reg.nii.gz -bin 0.5 -o {output_dir}/{(fname_seg.split("/")[-1]).split(".nii.gz")[0]}_reg_tresh.nii.gz -v 0'
+        )
 
         # Opening registered segmentation
         data_seg_reg = Image(output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_reg_tresh.nii.gz").data
@@ -135,12 +144,12 @@ def main(args=None):
             filewriter.writerow(["dice_std", np.std(dice_slice)])
         os.chdir(cwd)
 
-        generate_qc(fname_in1=fname_image, fname_in2=output_dir + "/template2anat.nii.gz", fname_seg=fname_seg, args=args,
-                    path_qc=path_qc, dataset=None, subject=None,
-                    process='sct_register_to_template')
+        # generate_qc(fname_in1=fname_image, fname_in2=output_dir + "/template2anat.nii.gz", fname_seg=fname_seg, args=args,
+        #             path_qc=path_qc, dataset=None, subject=None,
+        #             process='sct_register_to_template')
 
 
 if __name__ == "__main__":
-    sct.init_sct()
+    
     # call main function
     main()
